f84d340754ff89bc885490e06e5bb2a53978e6fe
vinodkv2511 <vinodkv2511@gmail.com>
2018-11-22 22:44:31 +0530

    Refactored view Register View's post method and validate_required_input method to better structure and readability
    Signed-off-by: vinodkv2511 <vinodkv2511@gmail.com>

--- ToDo/views.py
+++ ToDo/views.py
@@ -18,83 +18,88 @@ class HelloWorld(APIView):
 # View class to Register users
 class Register(APIView):
     def post(self, request):
-        # This order of required params is important as they are used to set variables by index
+        # The order of required params is important as they are used to set variables by index
         required_params = ['username', 'password', 'email']
         try:
             data = request.data
+            # Checking if all the required parameters are available in data
             if all(key in data for key in required_params):
                 try:
-                    if self.validate_input(required_params[0], data[required_params[0]]):
-                        user_name = data[required_params[0]]
-                    else:
-                        raise ValidationError('Invalid username, it can\'t be empty')
-
-                    if self.validate_input(required_params[1], data[required_params[1]]):
-                        password = data[required_params[1]]
-                    else:
-                        raise ValidationError('Invalid Password, password should be at least 8 characters long')
-
-                    if self.validate_input(required_params[2], data[required_params[2]]):
-                        email = data[required_params[2]]
-                    else:
-                        raise ValidationError('Invalid Email')
-
+                    user_name = self.validate_required_input(required_params[0], data[required_params[0]])
+                    password = self.validate_required_input(required_params[1], data[required_params[1]])
+                    email = self.validate_required_input(required_params[2], data[required_params[2]])
                 except ValidationError as er:
                     return Response({"error": str(er.messages[0])}, status=status.HTTP_400_BAD_REQUEST)
 
-                # Now it is considered that the input is valid
-                # We need to make sure that the username doesn't exist already
-                if User.objects.filter(username=user_name).exists():
-                    return Response({"error": "Username already taken, please try with a different username"}, status=status.HTTP_400_BAD_REQUEST)
-                if User.objects.filter(email=email).exists():
-                    return Response({"error": "E-mail already in use, please try logging in instead"}, status=status.HTTP_400_BAD_REQUEST)
-
+                # Input is now considered valid
+                # Creating user object to store to DB
                 new_user = User()
                 new_user.username = user_name
                 new_user.password = make_password(password)
                 new_user.email = email
+
+                # Trying to set optional parameters if available
                 try:
                     new_user.first_name = data['firstname'] if data['firstname'] is not None else ""
-                except KeyError as err:
-                    print("Error while parsing firstname : "+str(err))
+                except KeyError:
+                    print("Error while parsing firstname ")
                 try:
                     new_user.last_name = data['lastname'] if data['lastname'] is not None else ""
-                except KeyError as err:
-                    print("Error while parsing lastname : " + str(err))
+                except KeyError:
+                    print("Error while parsing lastname")
 
                 new_user.save()
 
                 return Response({"status": "Success"}, status=status.HTTP_201_CREATED)
 
             else:
-                return Response({"error": "Required param(s) missing, Please include and retry again"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"error": "Required param(s) missing, Please include and retry again"},
+                                status=status.HTTP_400_BAD_REQUEST)
         except Exception as exp:
             print("Unexpected exception occurred: "+str(exp))
-            return Response({"error": "Unexpected error occurred, please report this to Admin"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({"error": "Unexpected error occurred, please report this to Admin"},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    def validate_input(self, param, value):
+    @staticmethod
+    def validate_required_input(param, value):
+        """
+        Function to validate the required input of post method
+
+        :param param: It can take one of the values from required param of post method
+        :param value: Value of the passed param
+        :return: value if value passes the validation criteria for the given param
+        :raises: ValidationError: if value doesn't pass the validation criteria for the given param
+        """
+
         if param == 'username':
             if value is not None and type(value) == str and len(value) > 0:
-                return True
+                if User.objects.filter(username=value).exists():
+                    raise ValidationError('Username already taken, please try with a different username')
+                return value
             else:
-                return False
+                raise ValidationError('Invalid username, it can\'t be empty')
+
         elif param == 'password':
             if value is not None and type(value) == str and len(value) >= 8:
-                return True
+                return value
             else:
-                return False
+                raise ValidationError('Invalid Password, password should be at least 8 characters long')
+
         elif param == 'email':
             if value is not None and type(value) == str and len(value) > 0:
                 try:
                     validate_email(value)
                 except ValidationError:
-                    return False
+                    raise ValidationError('Invalid Email')
                 else:
-                    return True
+                    if User.objects.filter(email=value).exists():
+                        raise ValidationError('E-mail already in use, please try logging in instead')
+                    return value
             else:
-                return False
+                raise ValidationError('Invalid Email')
+
         else:
-            return False
+            raise ValidationError('Invalid Input Param Passed')
 
 
 
