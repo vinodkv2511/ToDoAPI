9742c07cf01b9bc56dd03fc5178e306c4f4e1aec
vinodkv2511 <vinodkv2511@gmail.com>
2019-02-16 19:25:52 +0530

    Restructured models a little

--- ToDo/models.py
+++ ToDo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 
-class List(models.Model):
+class TaskList(models.Model):
 
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
@@ -12,29 +12,20 @@ class List(models.Model):
 class Task(models.Model):
 
     id = models.AutoField(primary_key=True)
-    list = models.ForeignKey('List', on_delete=models.CASCADE)  # Foreign key relation to List
+    list = models.ForeignKey('TaskList', on_delete=models.CASCADE)  # Foreign key relation to List
     name = models.CharField(max_length=50)
     description = models.TextField()
 
 
 class ListAccess(models.Model):
 
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-        )  # Foreign key relation to user model
+    # Foreign key relation to user model
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    list = models.ForeignKey(
-        'List', 
-        on_delete=models.CASCADE
-        )  # Foreign key relation to List
+    # Foreign key relation to List
+    list = models.ForeignKey('TaskList', on_delete=models.CASCADE)
 
-    role = models.CharField(  # this field provides access level of a user on a list
-        max_length=5,
-        choices=(
-            ('owner', 'owner'),
-            ('guest', 'guest')
-        )
-    )
+    # this field provides access level of a user on a list
+    role = models.CharField(max_length=5)
 
 
