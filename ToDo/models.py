f25473c8d787e8577aec61db1b4a948ec1dcde78
vinodkv2511 <vinodkv2511@gmail.com>
2019-03-23 17:38:13 +0530

    added done field to Task model

--- ToDo/models.py
+++ ToDo/models.py
@@ -14,6 +14,7 @@ class Task(models.Model):
     id = models.AutoField(primary_key=True)
     list = models.ForeignKey('TaskList', on_delete=models.CASCADE)  # Foreign key relation to List
     name = models.CharField(max_length=50)
+    done = models.BooleanField(default=False)
     description = models.TextField()
 
 
